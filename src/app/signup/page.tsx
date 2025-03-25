1d6eb331f0164ee853baf42d97d2268deae9d27a
vgab1 <vgabriel.sousa123@gmail.com>
2025-03-25 11:46:06 -0300

    usuario cadastrado no frontend

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,71 @@
+import Image from "next/image";
+import Link from "next/link";
+import styles from "../page.module.scss";
+import logoImg from "/public/devpizza.svg";
+import { api } from "@/services/api";
+import { redirect } from "next/navigation";
+
+export default function Signup() {
+  async function handleRegister(formData: FormData) {
+    "use server";
+    const name = formData.get("name");
+    const email = formData.get("email");
+    const password = formData.get("password");
+
+    if (name === "" || email === "" || password === "") {
+      console.log("Preencha todos os campos");
+      return;
+    }
+
+    try {
+      await api.post("/users", {
+        name,
+        email,
+        password,
+      });
+    } catch (err) {
+      console.log("error");
+      console.log(err);
+    }
+
+    redirect("/");
+  }
+
+  return (
+    <>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo da pizzaria" />
+        <section className={styles.login}>
+          <h1>Criando sua conta</h1>
+          <form action={handleRegister}>
+            <input
+              type="text"
+              required
+              name="name"
+              placeholder="Digite seu nome..."
+              className={styles.input}
+            />
+            <input
+              type="email"
+              required
+              name="email"
+              placeholder="Digite seu email..."
+              className={styles.input}
+            />
+            <input
+              type="password"
+              required
+              name="password"
+              placeholder="Crie uma senha..."
+              className={styles.input}
+            />
+            <button type="submit">Cadastrar</button>
+          </form>
+          <Link href="/" className={styles.text}>
+            Já possui uma conta? Faça o login
+          </Link>
+        </section>
+      </div>
+    </>
+  );
+}
